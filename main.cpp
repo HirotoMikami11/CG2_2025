994a34211524cd32cc42226743886e14200ed841
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2025-04-11 14:20:07 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <Windows.h>
 #include<cstdint>
+#include<string>
+#include<format>
 
 ///*-----------------------------------------------------------------------*///
 //																			//
-///								ウィンドウプロシージャ						   ///
+///						ウィンドウプロシージャここから						   ///
 //																			//
 ///*-----------------------------------------------------------------------*/
 
@@ -21,6 +23,53 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
+///*-----------------------------------------------------------------------*///
+//																			//
+///						ウィンドウプロシージャここまで						   ///
+//																			//
+///*-----------------------------------------------------------------------*/
+
+
+
+///*-----------------------------------------------------------------------*///
+//																			//
+///									ログを出す							   ///
+//																			//
+///*-----------------------------------------------------------------------*/
+
+// 出力ウィンドウに文字を出す関数
+void Log(const std::string& message) {
+	OutputDebugStringA(message.c_str());
+}
+
+// string -> wstringに変換する関数
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+// wstring -> stringに変換する関数
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
 
 ///*-----------------------------------------------------------------------*///
 //																			//
@@ -98,11 +147,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		}
 	}
-
-
-
-
-
 
 	return 0;
 
