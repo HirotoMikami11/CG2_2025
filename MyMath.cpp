224cbdcfce416cf19730b28a319b97c4c9e67fb3
LE2A_16_Mikami <146906574+HirotoMikami11@users.noreply.github.com>
2025-06-20 13:52:05 +0900

    イージング関数を追加

--- MyMath.cpp
+++ MyMath.cpp
@@ -11,6 +11,194 @@
 float Lerp(const float& min, const float& max, float t) { return min + (max - min) * t; };
 
 
+float EaseInSine(float x) {
+	return 1 - cosf((x * float(M_PI)) / 2);
+}
+
+float EaseOutSine(float x) {
+	return sinf((x * float(M_PI)) / 2);
+}
+
+float EaseInOutSine(float x) {
+	return -(cosf(float(M_PI) * x) - 1) / 2;
+}
+
+
+float EaseInQuad(float x) {
+	return x * x;
+}
+float EaseOutQuad(float x) {
+	return 1 - (1 - x) * (1 - x);
+}
+
+float EaseInOutQuad(float x) {
+	return x < 0.5f ? 2 * x * x : 1 - powf(-2 * x + 2, 2) / 2;
+}
+
+
+float EaseInCubic(float x) {
+	return x * x * x;
+}
+
+float EaseOutCubic(float x) {
+	return 1 - powf(1 - x, 3);
+}
+
+float EaseInOutCubic(float x) {
+	return x < 0.5f ? 4 * x * x * x : 1 - powf(-2 * x + 2, 3) / 2;
+}
+
+float EaseInQuart(float x) {
+	return x * x * x * x;
+}
+
+float EaseOutQuart(float x) {
+	return 1 - powf(1 - x, 4);
+}
+
+float EaseInOutQuart(float x) {
+	return x < 0.5f ? 8 * x * x * x * x : 1 - powf(-2 * x + 2, 4) / 2;
+}
+
+float EaseInQuint(float x) {
+	return x * x * x * x * x;
+}
+
+float EaseOutQuint(float x) {
+	return 1 - powf(1 - x, 5);
+}
+
+float EaseInOutQuint(float x) {
+	return x < 0.5f ? 16 * x * x * x * x * x : 1 - powf(-2 * x + 2, 5) / 2;
+}
+
+
+float EaseInExpo(float x) {
+	return x == 0 ? 0 : powf(2, 10 * x - 10);
+}
+
+float EaseOutExpo(float x) {
+	return x == 1 ? 1 : 1 - powf(2, -10 * x);
+}
+
+float EaseInOutExpo(float x) {
+	return x == 0
+		? 0
+		: x == 1
+		? 1
+		: x < 0.5f ? powf(2, 20 * x - 10) / 2
+		: (2 - powf(2, -20 * x + 10)) / 2;
+}
+
+
+float EaseInCirc(float x) {
+	return 1 - sqrtf(1 - powf(x, 2));
+}
+
+float EaseOutCirc(float x) {
+	return sqrtf(1 - powf(x - 1, 2));
+}
+
+float EaseInOutCirc(float x) {
+	return x < 0.5f
+		? (1 - sqrtf(1 - powf(2 * x, 2))) / 2
+		: (sqrtf(1 - powf(-2 * x + 2, 2)) + 1) / 2;
+}
+
+
+float EaseInBack(float x) {
+	const float c1 = 1.70158f;
+	const float c3 = c1 + 1.0f;
+
+	return c3 * x * x * x - c1 * x * x;
+}
+
+float EaseOutBack(float x) {
+	const float c1 = 1.70158f;
+	const float c3 = c1 + 1;
+
+	return 1 + c3 * powf(x - 1, 3) + c1 * powf(x - 1, 2);
+}
+
+float EaseInOutBack(float x) {
+	const float c1 = 1.70158f;
+	const float c2 = c1 * 1.525f;
+
+	return x < 0.5f
+		? (powf(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+		: (powf(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+}
+
+
+float EaseInElastic(float x) {
+	const float c4 = (2 * float(M_PI)) / 3;
+
+	return x == 0
+		? 0
+		: x == 1
+		? 1
+		: -powf(2, 10 * x - 10) * sinf((x * 10 - 10.75f) * c4);
+}
+
+float EaseOutElastic(float x) {
+	const float c4 = (2 * float(M_PI)) / 3;
+
+	return x == 0
+		? 0
+		: x == 1
+		? 1
+		: powf(2, -10 * x) * sinf((x * 10 - 0.75f) * c4) + 1;
+}
+
+float EaseInOutElastic(float x) {
+	const float c5 = (2 * float(M_PI)) / 4.5f;
+
+	return x == 0
+		? 0
+		: x == 1
+		? 1
+		: x < 0.5f
+		? -(powf(2, 20 * x - 10) * sinf((20 * x - 11.125f) * c5)) / 2
+		: (powf(2, -20 * x + 10) * sinf((20 * x - 11.125f) * c5)) / 2 + 1;
+}
+
+
+
+
+
+float EaseInBounce(float x) {
+	return 1 - EaseOutBounce(1 - x);
+}
+
+float EaseOutBounce(float x) {
+	const float n1 = 7.5625;
+	const float d1 = 2.75;
+
+	if (x < 1 / d1) {
+		return n1 * x * x;
+	} else if (x < 2 / d1) {
+		return n1 * (x -= 1.5f / d1) * x + 0.75f;
+	} else if (x < 2.5 / d1) {
+		return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+	} else {
+		return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+	}
+}
+
+float EaseInOutBounce(float x) {
+	return x < 0.5f
+		? (1 - EaseOutBounce(1 - 2 * x)) / 2
+		: (1 + EaseOutBounce(2 * x - 1)) / 2;
+}
+
+void ResetEase(float t, float EaseT, int dir)
+{
+	t = 0;
+	EaseT = 0;
+	dir = 1;
+}
+
+
 ///xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx//
 
 ///																		///
